52fd0c5340a2d53d86dd09b484e32e5db5258e55
KArtem <k6art5@gmail.com>
2025-11-15 13:20:06 +0700

    update App file

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,15 +1,13 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <post-wrapper />
 </template>
 
-<style scoped>
+<script>
+import PostWrapper from "@/components/Posts/PostWrapper.vue";
+import {defineComponent} from "vue";
 
-</style>
+export default defineComponent({
+  components: {PostWrapper}
+})
+
+</script>
