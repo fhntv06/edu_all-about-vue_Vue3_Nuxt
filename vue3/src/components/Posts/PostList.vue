01fde1697b79d47ee6f6d2e30c918a52d68dc9b5
KArtem <k6art5@gmail.com>
2025-11-15 13:18:34 +0700

    create component PostList

---
+++ vue3/src/components/Posts/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold text-gray-800 mb-6">Список постов</h2>
+    <div v-if="posts.length" v-for="post in posts">
+      <post-item :key="post.id" :post="post" @deletePost="deletePost" />
+    </div>
+    <h3 v-else>Список постов пуст</h3>
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/Posts/PostItem.vue";
+
+export default {
+  components: {PostItem},
+  props: {
+    posts: { type: Array, default: [] }
+  },
+  methods: {
+    deletePost(post) {
+      this.$emit('deletePost', post)
+    }
+  }
+}
+</script>
