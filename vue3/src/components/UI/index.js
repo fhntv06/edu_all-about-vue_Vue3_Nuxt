5fc2a79e05f0b4e794413d3b878701e23aab13ee
KArtem <k6art5@gmail.com>
2025-11-15 13:24:12 +0700

    create index file for components registration

---
+++ vue3/src/components/UI/index.js
@@ -0,0 +1,8 @@
+import Field from "@/components/UI/Field.vue";
+import Loader from "@/components/UI/Loader.vue";
+import Modal from "@/components/UI/Modal.vue";
+import Select from "@/components/UI/Select.vue";
+
+export default [
+  Field, Loader, Modal, Select
+]
