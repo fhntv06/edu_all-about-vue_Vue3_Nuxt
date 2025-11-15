ec03901c84be24923364e382aff84458eb551911
KArtem <k6art5@gmail.com>
2025-11-15 13:23:05 +0700

    update entrance app in main.js file, component registration UI

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import components from '@/components/UI'
 import App from './App.vue'
 import router from './router'
+import './index.css';
 
 const app = createApp(App)
+
+components.forEach((component) => app.component(component.name, component))
 
 app.use(createPinia())
 app.use(router)
