0fb62d2f1e5f3b615bf94b4e12d13b65fc1572a7
KArtem <k6art5@gmail.com>
2025-11-15 17:50:34 +0700

    create imports new components UI

--- vue3/src/components/UI/index.js
+++ vue3/src/components/UI/index.js
@@ -3,7 +3,9 @@ import Field from "@/components/UI/Field.vue";
 import Filter from "@/components/UI/Filter.vue";
 import Loader from "@/components/UI/Loader.vue";
 import Modal from "@/components/UI/Modal.vue";
+import Select from "@/components/UI/Select.vue";
+import Pagination from "@/components/UI/Pagination.vue";
 
 export default [
-  Button, Field, Filter, Loader, Modal
+  Button, Field, Filter, Loader, Modal, Select, Pagination
 ]
