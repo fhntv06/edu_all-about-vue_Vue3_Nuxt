8620c06220c8ece5d9cd0538ffad19e0f1fb4b47
KArtem <k6art5@gmail.com>
2025-12-10 18:42:07 +0700

    import stores in main.js

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import components from '@/components/UI'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import stores from '@/stores'
 import './index.css';
 
 const app = createApp(App)
 
 components.forEach((component) => app.component(component.name, component))
+stores.forEach((store) => app.use(store))
 
 app.use(createPinia())
 app.use(router)
