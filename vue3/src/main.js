170f829680b8c57987bd74cef49ab64b1ba0245b
KArtem <k6art5@gmail.com>
2025-12-16 13:10:28 +0700

    delete package pinia

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import components from '@/components/UI'
 import App from '@/App.vue'
 import router from '@/router'
@@ -11,7 +10,6 @@ const app = createApp(App)
 components.forEach((component) => app.component(component.name, component))
 app.use(store)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
